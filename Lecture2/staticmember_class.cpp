f4b6c37cdf634651715683d06e68f706f47811ae
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-08-07 07:59:23 +0545

    static member function

--- Lecture2/staticmember_class.cpp
+++ Lecture2/staticmember_class.cpp
@@ -3,27 +3,31 @@ using namespace std;
 class info{
   public:
   float english,maths,science;
-//   Declare a static variable
+//   declare a static variable
   static int age;
-  void set_data(float e,float m,float s){
+  void set_value(float e,float m,float s){
       english = e;
       maths = m;
       science = s;
   }
+  void show_value(){
+      cout<<english<<","<<maths<<","<<science<<endl;
+  }
 };
-// Define a static variable
+// defining a static variable
 int info::age = 23;
-
 int main(){
     info i1,i2,i3;
-    i1.set_data(85,90,67);
-    i2.set_data(80,92,78);
-    i3.set_data(58,80,65);
-    cout<<i1.english<<" "<<i1.maths<<" "<<i1.science<<endl;
-    cout<<i2.english<<" "<<i2.maths<<" "<<i2.science<<endl;
-    cout<<i3.english<<" "<<i3.maths<<" "<<i3.science<<endl;
-    cout<<"Age: "<<i1.age<<endl;
-    cout<<"Age: "<<i2.age<<endl;
-    cout<<"Age: "<<i3.age<<endl;
+    i1.set_value(45,60,70);
+    i2.set_value(50,66,77);
+    i3.set_value(60,70,80);
+    i1.show_value();
+    i2.show_value();
+    i3.show_value();
+    cout<<i1.age<<endl;
+    cout<<i2.age<<endl;
+    cout<<i3.age<<endl;
+    cout<<info::age<<endl;
+    
     return 0;
 }
