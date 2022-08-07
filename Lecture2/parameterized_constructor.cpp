617a370fd66e342703289bad3c185e5c7d54d52c
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-08-07 07:32:48 +0545

    Lecture 2

--- Lecture2/parameterized_constructor.cpp
+++ Lecture2/parameterized_constructor.cpp
@@ -1,33 +1,23 @@
-// C++ program to calculate the area of a wall
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-// declare a class
-class Wall {
-  private:
-    double length;
-    double height;
-
-  public:
-    // parameterized constructor to initialize variables
-    Wall(double len, double hgt) {
-      length = len;
-      height = hgt;
+class box{
+    private:
+    int length;
+    int breadth;
+    int height;
+    public:
+    box(int l , int b,int h){
+        length = l;
+        breadth = b;
+        height = h;
     }
-
-    double calculateArea() {
-      return length * height;
+    void show_data(){
+        cout<<length<<","<<breadth<<","<<height<<endl;
     }
 };
 
-int main() {
-  // create object and initialize data members
-  Wall wall1(10.5, 8.6);
-  Wall wall2(8.5, 6.3);
-
-  cout << "Area of Wall 1: " << wall1.calculateArea() << endl;
-  cout << "Area of Wall 2: " << wall2.calculateArea();
-
-  return 0;
+int main(){
+    box b1(3,4,5);
+    return 0;
+    
 }
